827fa822e4b139d30a25e9138a2e80d83d6f8637
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-12 16:56:59 +0100

    global colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'first': '#210535',
+        'second': '#440C4D',
+        'third': '#7B337D',
+        'fourth': '#C973B4',
+        'fifth': '#F5D5E0'
+      }
+    },
   },
   plugins: [],
 }
