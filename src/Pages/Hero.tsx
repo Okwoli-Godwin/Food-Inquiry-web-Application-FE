6cbdaaea855e19c4c1b62d67874f6e5f14984406
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-13 00:02:22 +0100

    hero updates

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -4,8 +4,11 @@ const Hero = () => {
   return (
       <section>
         <div className="h-[100vh] w-full bg-[red] bg-hero bg-center bg-no-repeat bg-cover relative">
-          <div className="absolute w-[100%] h-[100%] left-0 top-0 right-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+          <div className="flex justify-center items-center absolute w-[100%] h-[100%] left-0 top-0 right-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
             
+            <div className="w-[55%] h-[300px] border-gray-400 border">
+                
+            </div>
           </div>
         </div>
       </section>
