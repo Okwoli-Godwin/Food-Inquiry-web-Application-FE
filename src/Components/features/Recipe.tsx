397e494d3b31fcccc04a25a4d944cf64772dbab7
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-08-13 18:59:12 +0100

    working on the single recipe

--- src/Components/features/Recipe.tsx
+++ src/Components/features/Recipe.tsx
@@ -1,5 +1,12 @@
+import { RecipeDetails, RecipeImage } from ".";
+
 const Recipe = () => {
-  return <div className="bg-white w-[95%]">Recipe</div>;
+  return (
+    <div className="bg-white w-[95%] p-5 grid grid-cols-[0.7fr,1fr] gap-9 max-md:grid-cols-1 max-md:w-full">
+      <RecipeImage />
+      <RecipeDetails />
+    </div>
+  );
 };
 
 export default Recipe;
