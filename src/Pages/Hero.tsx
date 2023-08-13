d048eb35225da9bc777860ea282958d225a89aaf
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-13 01:49:31 +0100

    update

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -6,8 +6,8 @@ const Hero = () => {
         <div className="h-[100vh] w-full bg-[red] bg-hero bg-center bg-no-repeat bg-cover relative">
           <div className="flex justify-center items-center absolute w-[100%] h-[100%] left-0 top-0 right-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
             
-            <div className="w-[55%] h-[300px] border-gray-400 border">
-                
+            <div className="w-[95%] h-[300px] border-[#fff] border-b border-r border-l flex-col items-center flex">
+                <h3 className="font-great-vibes text-[60px] p-0 text-[#F4A01B]">Welcome</h3>
             </div>
           </div>
         </div>
