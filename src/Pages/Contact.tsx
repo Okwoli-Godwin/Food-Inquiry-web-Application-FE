e06764a87e27a11ceaf98415570a6e650e3474b4
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-16 00:51:14 +0100

    contact us mobile done

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -9,26 +9,26 @@ const Contact = () => {
   return (
       <section style={{overflow: "hidden"}}>
             <div className="h-[100%] w-[100%] bg-recipehero bg-center bg-no-repeat bg-cover relative">
-              <div className="w-[100%] h-[100%] pt-[200px] pb-[200px] flex-col flex justify-center items-center md:pt-[100px] md:pb-[100px]" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                  <h1 className="text-[#fff] text-[60px] font-bold">Contact Us</h1>
-                  <p className="text-[#fff] text-[30px] mt-[10px]">Home || Contact us</p>
+              <div className="w-[100%] h-[100%] pt-[200px] pb-[200px] flex-col flex justify-center items-center md:pt-[200px] md:pb-[100px]" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                  <h1 className="text-[#fff] text-[60px] font-bold md:text-[40px]">Contact Us</h1>
+                  <p className="text-[#fff] text-[30px] mt-[10px] md:text-[25px]">Home || Contact us</p>
             </div>
           </div>
           
-          <div className="w-[100%] flex justify-center items-center h-[100%] mt-[30px]">
-              <div className="w-[200px] h-[100%] bg-third rounded p-[20px] flex-col ml-[25px]">
+          <div className="w-[100%] flex justify-center items-center h-[100%] mt-[30px] flex-wrap">
+              <div className="w-[200px] h-[100%] bg-third rounded p-[20px] flex-col ml-[25px] md:mb-[15px]">
                   <div className="text-[#fff]"><BsTelephoneFill /></div>
                   <h3 className="text-[#fff] text-[20px] font-semibold mt-[22px]">Phone us</h3>
                   <p className="text-[#fff] mt-[7px]">+0123 (456) 789</p>
               </div>
 
-              <div className="w-[200px] h-[100%] bg-third rounded p-[20px] flex-col ml-[25px]">
+              <div className="w-[200px] h-[100%] bg-third rounded p-[20px] flex-col ml-[25px] md:mb-[15px]">
                   <div className="text-[#fff] text-[28px]"><MdEmail /></div>
                   <h3 className="text-[#fff] text-[20px] font-semibold mt-[22px]">Email us</h3>
                   <p className="text-[#fff] mt-[7px]">support@gmail.com</p>
               </div>
 
-              <div className="w-[200px] h-[100%] bg-third rounded p-[20px] flex-col ml-[25px]">
+              <div className="w-[200px] h-[100%] bg-third rounded p-[20px] flex-col ml-[25px] md:mb-[15px]">
                   <div className="text-[#fff]"><MdLocationPin /></div>
                   <h3 className="text-[#fff] text-[20px] font-semibold mt-[22px]">Location</h3>
                   <p className="text-[#fff] mt-[7px]">25 main road, Lagos</p>
@@ -36,12 +36,12 @@ const Contact = () => {
           </div>
 
           <div className="w-[100%] flex justify-center mt-[70px]">
-              <div className="w-[70%] flex flex-wrap ">
-              <div className="w-[48%]">
+              <div className="w-[70%] flex flex-wrap">
+              <div className="w-[48%] lg:w-[100%] lg:justify-center">
                   <img src={img} alt="" />
               </div>
               
-              <div className="w-[48%] flex flex-col">
+              <div className="w-[48%] flex flex-col lg:w-[100%] lg:mt-[25px]">
                 <h3 className="text-[26px] font-semibold">Send Us Message</h3>
                 <input 
                     type="text" 
