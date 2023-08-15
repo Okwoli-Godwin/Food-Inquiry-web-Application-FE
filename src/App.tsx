cc521120f0dd8c5529890120709f471099810c93
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-15 06:39:54 +0100

    Signin in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { main } from "./routes/Routes"
+import Text from "./Pages/Text"
 
 const App = () => {
   return (
     <RouterProvider router={main}/>
+    // <Text />
   )
 }
 
