29f69691cb0b24152553e53f217cff38a09aad63
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-15 13:25:59 +0100

    landing page done for now

--- src/Components/Static/Footer.tsx
+++ src/Components/Static/Footer.tsx
@@ -1,8 +1,59 @@
 
 
 const Footer = () => {
+
+     const footerNavs = [
+        {
+            href: 'javascript:void()',
+            name: 'About'
+        },
+        {
+            href: 'javascript:void()',
+            name: 'Blog'
+        },
+        {
+            href: 'javascript:void()',
+            name: ''
+        },
+        {
+            href: 'javascript:void()',
+            name: 'Team'
+        },
+        {
+            href: 'javascript:void()',
+            name: 'Careers'
+        },
+
+        {
+            href: 'javascript:void()',
+            name: 'Suuport'
+        }
+    ]
   return (
-    <div>Footer</div>
+     <footer className="w-[100%] h-[100%] flex bg-footer bg-no-repeat bg-center bg-cover mt-[80px]">
+          <div
+              className="w-[100%] h-[100%] flex justify-center items-center pt-[80px] pb-[20px] flex-col"
+              style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}
+          >
+              <img src="http://unionagency.one/delice/img/logo.png" className="h-[40px]" />
+
+              <div className="flex items-center mt-[35px] lg:hidden">
+            <h3 className="mr-[55px] m-[10px] text-[15px] text-[#fff] font-medium cursor-pointer border-b-2 border-transparent hover:border-yellow-500 transition transform hover:scale-x-100">
+              Home
+            </h3>
+            <h3 className="mr-[55px] m-[10px] text-[15px] text-[#fff] font-medium cursor-pointer border-b-2 border-transparent hover:border-yellow-500 transition transform hover:scale-x-100">
+              Our Menus
+            </h3>
+            <h3 className="mr-[55px] m-[10px] text-[15px] text-[#fff] font-medium cursor-pointer border-b-2 border-transparent hover:border-yellow-500 transition transform hover:scale-x-100">
+              About Us
+            </h3>
+            <h3 className="mr-[55px] m-[10px] text-[15px] text-[#fff] font-medium cursor-pointer border-b-2 border-transparent hover:border-yellow-500 transition transform hover:scale-x-100">
+              Contact Us
+            </h3>
+              </div>
+              <div className="text-[#fff] text-[15px] mt-[35px]">© 2023 Miss Ore Foon inquiry application</div>
+        </div>
+    </footer>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,9 @@ export default {
 
         'signinbg': "url('https://media.istockphoto.com/id/1128915783/photo/delicious-appetizing-classic-spaghetti-pasta-with-tomato-sauce-parmesan-cheese-and-fresh.jpg?s=612x612&w=0&k=20&c=UqLauvhesSHXpjA4FgOsPbjmuAytffqnhwEl0Mt5eHU=')",
 
-        'venture': "url('https://media.istockphoto.com/id/1394055240/photo/happy-black-female-chef-preparing-food-in-frying-pan-at-restaurant-kitchen.jpg?s=612x612&w=0&k=20&c=6DjpoYqgYVDLmtj3-q7H7wvoiwkVgzi1rn7a_XUZ_Ng=')"
+        'venture': "url('https://media.istockphoto.com/id/1394055240/photo/happy-black-female-chef-preparing-food-in-frying-pan-at-restaurant-kitchen.jpg?s=612x612&w=0&k=20&c=6DjpoYqgYVDLmtj3-q7H7wvoiwkVgzi1rn7a_XUZ_Ng=')",
+
+        'footer': "url('https://media.istockphoto.com/id/1312307361/photo/rosemary-pepper-and-salt-cooking-border.jpg?s=612x612&w=0&k=20&c=6LqSjh4Qc_pmmUm1wa73AOPgDV5aBS9zbV90LisJ3M0=')"
       },
       colors: {
         'first': '#210535',
