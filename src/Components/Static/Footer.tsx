d45b9716a6990c147f1cc9dcb6f6b706f692bac7
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-15 23:57:40 +0100

    updating mobile screens

--- src/Components/Static/Footer.tsx
+++ src/Components/Static/Footer.tsx
@@ -32,12 +32,12 @@ const Footer = () => {
   return (
      <footer className="w-[100%] h-[100%] flex bg-footer bg-no-repeat bg-center bg-cover mt-[80px]">
           <div
-              className="w-[100%] h-[100%] flex justify-center items-center pt-[80px] pb-[20px] flex-col"
+              className="w-[100%] h-[100%] flex justify-center items-center pt-[80px] pb-[20px] flex-col md:pl-[13px] md:items-start"
               style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}
           >
               <img src="http://unionagency.one/delice/img/logo.png" className="h-[40px]" />
 
-              <div className="flex items-center mt-[35px]">
+              <div className="flex items-center mt-[35px] md:flex-col md:items-start">
             <h3 className="mr-[55px] m-[10px] text-[15px] text-[#fff] font-medium cursor-pointer border-b-2 border-transparent hover:border-yellow-500 transition transform hover:scale-x-100">
               Home
             </h3>
--- src/Pages/Adventure.tsx
+++ src/Pages/Adventure.tsx
@@ -4,10 +4,10 @@
 const Adventure = () => {
     return (
         <div className="w-[100%] h-[100%] bg-[red] bg-venture bg-no-repeat bg-center bg-cover bg-fixed">
-            <div className="w-[100%] h-[100%] flex flex-col pl-[50px] pt-[150px] pb-[150px]" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+            <div className="w-[100%] h-[100%] flex flex-col pl-[50px] pt-[150px] pb-[150px] md:items-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
                 <h3 className="text-[#fff] text-[25px] font-semibold">Our Restaurant</h3>
-                <h1 className="text-[#fff] text-[50px] font-semibold mt-[15px]">Delicious food with meaning</h1>
-                <p className="text-[#fff] w-[350px] mt-[15px] text-[17px]">
+                <h1 className="text-[#fff] text-[50px] font-semibold mt-[15px] md:text-[40px] md:text-center md:leading-tight">Delicious food with meaning</h1>
+                <p className="text-[#fff] w-[350px] mt-[15px] text-[17px] md:text-center">
                     On a cold winter night, brighten up your table with a big, colorful mix of Recipies.
                 </p>
 
