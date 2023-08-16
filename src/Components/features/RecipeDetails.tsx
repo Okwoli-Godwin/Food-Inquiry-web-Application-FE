bbc9d035b90fd929bd96e8fddcc141a76922e19f
Okwoli-Godwin <okwolig60@gmail.com>
2023-08-16 06:23:20 +0100

    update

--- src/Components/features/RecipeDetails.tsx
+++ src/Components/features/RecipeDetails.tsx
@@ -6,11 +6,6 @@ const RecipeDetails = () => {
       </h2>
       <hr />
       {/* category button */}
-      <div>
-        <button className="py-1 px-8 border-[3px] border-first rounded-full">
-          recipe
-        </button>
-      </div>
       {/* calorie count */}
       <div className="flex gap-2">
         <h3 className="text-2xl text-gray-600">calorie count:</h3>
@@ -27,14 +22,39 @@ const RecipeDetails = () => {
       <hr />
 
       {/* ingredients */}
-      <div>
-        <label className="text-2xl font-medium">Ingredients</label>
-        <ul className="list-disc text-lg my-2 mx-5">
-          <li>an ingredient </li>
-          <li>an ingredient </li>
-          <li>an ingredient </li>
-          <li>an ingredient </li>
-        </ul>
+      <div className=" w-[100%] flex">
+        <div className="w-[40%] flex flex-col border border-[lightgray] p-[20px]">
+          <h3 className="text-[23px] font-semibold">Recipies</h3>
+          <p className="mt-[15px]">1 Cup sifted all purpose flour</p>
+          <p className="mt-[8px]">4 Large eggs</p>
+          <p className="mt-[8px]">5 Cups of mineral water</p>
+          <p className="mt-[8px]">Pepper</p>
+          <p className="mt-[8px]">Tomatoes</p>
+        </div>
+
+        <div className="w-[40%] flex flex-col border border-[lightgray] p-[20px] ml-[20px]">
+          <h3 className="text-[23px] font-semibold">Nutrition</h3>
+          <div className="mt-[15px] w-[100%] flex justify-between">
+            <p>Calories</p>
+            <p>329</p>
+          </div>
+          <div className="mt-[15px] w-[100%] flex justify-between">
+            <p>Sugar</p>
+            <p>10.5g</p>
+          </div>
+          <div className="mt-[15px] w-[100%] flex justify-between">
+            <p>Protein</p>
+            <p>22.5g</p>
+          </div>
+          <div className="mt-[15px] w-[100%] flex justify-between">
+            <p>Fat</p>
+            <p>02g</p>
+          </div>
+          <div className="mt-[15px] w-[100%] flex justify-between">
+            <p>Carbs</p>
+            <p>18g</p>
+          </div>
+        </div>
       </div>
     </div>
   );
