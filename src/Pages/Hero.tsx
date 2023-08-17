c7e36905d72b1c2ae57bdc2fccfdcae5db10309f
Damilola Sanni <sannifortune11@gmail.com>
2023-08-17 22:48:44 +0100

    update

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -1,84 +1,13 @@
-// import {CiSearch} from "react-icons/ci"
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Hero2 from "./Hero2";
-// import Hero3 from "./Hero3"
-// import { useMutation } from "react-query";
-// import { useQuery } from "@tanstack/react-query";
-// import { searcheRcipes } from "../apis/UserApi/userApi";
-// import { useState } from "react";
-
-// const Hero = () => {
-
-//     const settings = {
-//     infinite: true,
-//     speed: 1000,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//   };
-
-//   const [search, setSearch] = useState("")
- 
-// const data = useQuery({
-//     queryFn: () => searcheRcipes(search),
-//   });
-
-//   console.log("data", data);
-  
-
-//   return (
-//       <section style={{overflow: "hidden"}}>
-//         <Slider {...settings}>
-//             <div className="h-[100%] w-[100%] bg-hero bg-center bg-no-repeat bg-cover relative">
-//               <div className="w-[100%] h-[100%] pt-[200px] pb-[200px] flex justify-center items-center md:pt-[100px] md:pb-[100px]" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                  
-//                   <div className="w-[95%] flex-col items-center flex md:mt-[13px]">
-//                 <h3 className="font-great-vibes text-[60px] p-0 text-[#F4A01B] md:text-[30px]">Welcome</h3>
-//                 <h2 className="text-[#fff] text-[47px] font-bold mt-[12px] sm:text-sm md:text-[32px]">HIGH QUALITY MEALS</h2>
-//                 <p className="text-[#b9b9b9] text-[20px] w-[650px] text-center mt-[12px] md:text-[16px] md:w-[350px]">
-//                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.
-//                 </p>
-                
-//                 <div className="w-[600px] md:w-[350px] h-[60px] md:h-[50px] flex bg-[#fff] mt-[23px] rounded-full items-center overflow-hidden ">
-//                     <div className="w-[10%] md:w-[15%] h-[100%] flex justify-center items-center">
-//                         <div className="text-[24px]">
-//                             <CiSearch />
-//                         </div>
-//                     </div>
-//                 <input
-                  
-//                   onChange={(e) => {
-//                     setSearch(e.target.value)
-//                     }}
-//                   type="text" className="flex-1 h-[100%] border-none outline-none" placeholder="Search recipes......" />
-//                 </div>
-//             </div>
-//             </div>
-//               </div>
-              
-//               <Hero2 />
-            
-//               <Hero3 />
-//         </Slider>
-//       </section>
-//   )
-// }
-
-// export default Hero
-
-
-
 import { CiSearch } from "react-icons/ci";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Hero2 from "./Hero2";
 import Hero3 from "./Hero3";
+import { useMutation } from "react-query";
 import { useQuery } from "@tanstack/react-query";
-import { searcheRcipes } from "../apis/UserApi/userApi";
 import { useState } from "react";
+import { searcheRcipes } from "../apis/RecipiesApi/RecipiesApi";
 
 const Hero = () => {
   const settings = {
@@ -91,17 +20,10 @@ const Hero = () => {
 
   const [search, setSearch] = useState("");
 
-  const { data, isLoading, isError } = useQuery({
+  const data = useQuery({
     queryFn: () => searcheRcipes(search),
   });
 
-  
-  const handleKeyPress = (e: any) => {
-    if (e.key === "Enter") {
-      setSearch(e.target.value);
-    }
-  };
-  console.log("data", data);
 
   return (
     <section style={{ overflow: "hidden" }}>
@@ -130,7 +52,6 @@ const Hero = () => {
                   </div>
                 </div>
                 <input
-                  onKeyPress={handleKeyPress}
                   onChange={(e) => {
                     setSearch(e.target.value);
                   }}
@@ -142,9 +63,11 @@ const Hero = () => {
             </div>
           </div>
         </div>
+
+        {/* <Hero2 />
+
+        <Hero3 /> */}
       </Slider>
-      {/* <Hero2 />
-      <Hero3 /> */}
     </section>
   );
 };
--- src/apis/RecipiesApi/RecipiesApi.tsx
+++ src/apis/RecipiesApi/RecipiesApi.tsx
@@ -1,9 +1,16 @@
 import axios from "axios";
 
-const url = "https://food-inquiry-web-79bv.onrender.com/api/recipes";
+const url = "https://food-inquiry.onrender.com/api/recipes";
 
 export const createRecipes = async (recipes: any) => {
   return await axios
     .post(`${url}/createrecipe`, recipes)
     .then((res) => res.data);
+};
+export const searcheRcipes = async (searchValue: string) => {
+  return await axios
+    .get(
+      `https://food-inquiry.onrender.com/api/recipes/viewAllSearchedRecipes?title=${searchValue}`
+    )
+    .then((res) => res);
 };
